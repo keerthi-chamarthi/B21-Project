8498ce65bb50c018258a6d551fee946456aa9a7f
Ushasri Mavuri <ushasrimavuri1999@gmail.com>
2020-06-20 16:08:10 +0530

    added uprofile page

--- sampleagfinal/src/app/response-page/response-page.component.ts
+++ sampleagfinal/src/app/response-page/response-page.component.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-response-page',
   templateUrl: './response-page.component.html',
-  styleUrls: ['./response-page.component.scss']
+  styleUrls: ['./response-page.component.scss'],
 })
 export class ResponsePageComponent implements OnInit {
+  faUser = faUser;
   public displayName: string = '';
-  constructor(private obj: ActivatedRoute) { }
+  constructor(private obj: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.obj.paramMap.subscribe(
       (params: ParamMap) => (this.displayName = String(params.get('name')))
     );
   }
-
 }
